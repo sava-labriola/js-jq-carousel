07174fba0b98dcd01aeb2449bc6a3075348a163c
sava-labriola <labriolasava00@gmail.com>
2020-05-04 15:04:43 +0200

    Scorrimento automatico + play pausa con pulsanti

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//slider che permette di scorrere le immagini avanti e dietro
+//slider che permette di scorrere le immagini avanti e dietro.
+//scorrimentoautomatico e pausa con dei pulsanti.
 
 $('.next').click(function() {
     var current_image = $('.active');
@@ -32,4 +33,28 @@ $('.prev').click(function() {
         $('img:nth-child(4)').addClass('active');
         $('i:nth-child(4)').addClass('selected');
     }
+})
+var clock;
+$('.start').click(function () {
+    clock = setInterval(automatic_slider, 3000);
+    function automatic_slider(){
+        var current_image = $('.active');
+        current_image.removeClass('active');
+        var next_image = current_image.next('img');
+        var current_bullet = $('.selected');
+        current_bullet.removeClass('selected');
+        var next_bullet = current_bullet.next('i');
+        if (next_image.length != 0) {
+            next_image.addClass('active');
+            next_bullet.addClass('selected');
+        }
+        else {
+            $('img:first-child').addClass('active');
+            $('i:first-child').addClass('selected');
+        }
+    }
+})
+
+$('.pause').click(function () {
+    clearInterval(clock);
 })
